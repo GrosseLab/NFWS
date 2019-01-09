d7be8730bb1ac3aa68f53dde1bd4fd16e801d219
Martin Porsch <martin.porsch@informatik.uni-halle.de>
2019-01-09 10:14:44 +0100

    removed unneccasary use if annotatyion in salmon. Salmon gene summary is not recommended anyway. use tximport instead

--- rules/de/salmon.py
+++ rules/de/salmon.py
@@ -8,7 +8,7 @@ else:
 	bam_file = tempfile.mkstemp(prefix="./", suffix=".bam")[1]
 	system("samtools merge -f " + bam_file + " " + " ".join(snakemake.input.bams))
 
-system("salmon quant --threads " + str(snakemake.threads) + " --targets " + snakemake.input.transcriptome + " --alignments " + bam_file + " --geneMap " + snakemake.input.gtf + " " + " ".join(snakemake.params))
+system("salmon quant --threads " + str(snakemake.threads) + " --targets " + snakemake.input.transcriptome + " --alignments " + bam_file + " " + " ".join(snakemake.params))
 
 if len(snakemake.input.bams) > 1:
 	remove(bam_file)
